da99df423ac54882da4d987a0f52b8d2a031d86a
Agnieszka Mor <agnieszka.mor@iver.pl>
2020-10-14 08:35:19 +0200

    Get a list of speakers

---
+++ src/Controllers/SpeakersController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using CoreCodeCamp.Data;
+using CoreCodeCamp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpeakersController : ControllerBase
+    {
+        private readonly ICampRepository _repository;
+        private readonly IMapper _mapper;
+
+        public SpeakersController(ICampRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<SpeakerModel[]>> Get()
+        {
+            try
+            {
+                var results = await _repository.GetAllSpeakersAsync();
+
+                return _mapper.Map<SpeakerModel[]>(results);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Database Failure");
+            }
+        }
+    }
+}
